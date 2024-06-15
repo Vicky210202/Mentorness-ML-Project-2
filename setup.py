e996384b4f68c4e189a29d7978fb108cf2be5ee5
Vicky210202 <vivic210202@gmail.com>
2024-06-16 00:50:59 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+e_dot = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''This functions returns the list requirements'''
+    requirements = list()
+    with open(file_path) as req_file:
+        requirements = req_file.readlines()
+        requirements = [reqmt.replace('\n', '') for reqmt in requirements]
+
+        if e_dot in requirements:
+            requirements.remove(e_dot)
+
+    return requirements    
+
+setup(
+    name = 'Fastag Fraud Detection - Mentorness Internship',
+    version = '1.0.0',
+    author = 'Vignesh S',
+    author_email = 'vivic210202@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
