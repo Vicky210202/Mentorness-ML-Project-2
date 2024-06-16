0219df6f1f817206870757b69ebff69482b80bec
Vicky210202 <vivic210202@gmail.com>
2024-06-16 19:26:48 +0530

    data ingestion created

---
+++ src/components/model.py
@@ -0,0 +1,10 @@
+from src.components.data_ingestion import DataIngestion
+
+
+if __name__ == "__main__" :
+    
+    # data ingestion
+    data_obj = DataIngestion()
+    data_obj.initiate_data_ingestion()
+
+
