3174e66be6183ac779c848dd2db32028bdd6a944
Vicky210202 <vivic210202@gmail.com>
2024-06-17 19:25:23 +0530

    sql script modified

--- src/sql/script.sql
+++ src/sql/script.sql
@@ -4,11 +4,11 @@ CREATE DATABASE mentorness;
 -- Creating table 'fastag' in PostgreSQL 
 DROP TABLE fastag;
 CREATE TABLE fastag (
-    id SERIAL PRIMARY KEY,
-    vehicle_type TEXT,
-    vehicle_plate_number TEXT,
-    amount_frauded NUMERIC,
-    fraud_indicator TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    vehicle_type TEXT NOT NULL,
+    vehicle_plate_number TEXT NOT NULL,
+    amount_frauded NUMERIC NOT NULL,
+    fraud_indicator TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
