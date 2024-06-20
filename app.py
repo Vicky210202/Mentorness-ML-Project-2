be871bf3dfbddd371aa3cf7fd174ebeee73620bd
Vignesh S <vivic210202@gmail.com>
2024-06-20 23:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,7 @@ def predict():
         
         return render_template('index.html', results = prediction[0])
     except Exception as e:
-        raise CustomException(e, sys)
-
+        return e
 if __name__ == '__main__':
     app.run(debug=True)
     
